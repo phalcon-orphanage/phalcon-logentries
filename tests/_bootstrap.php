94bcd4c6993bfbd366f7684543b61e25a85d26c3
Serghei Iakovlev <serghei@phalconphp.com>
2016-08-14 11:47:36 +0300

    Cleaned tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+error_reporting(-1);
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
@@ -29,4 +31,4 @@ if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
     );
 }
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
